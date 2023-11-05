f666c32119cbb6b9ed23fc87be38f13d923e958f
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-11-05 12:05:18 -0300

    actions

--- backend/test/taskTests.js
+++ backend/test/taskTests.js
@@ -12,77 +12,72 @@ describe('Testes das rotas de tasks:', function() {
   this.timeout(5000); 
   //Teste de busca das tasks
   let id;
-
-  it('/GET', function (done) {
-    request(app)
-      .get('/tasks')
-      .end(function (err, response) {
-        if (err) {
-          console.log("Erro: " + err);
-          return done(err);
-        }
-        response.should.have.status(200);
-        done(); // Chama done para indicar que o teste está concluído.
-      });
+  it('/GET', async () => { 
+  try{
+    const tasks = [];
+    const response = await request(app)
+    .get('/tasks');
+    response.should.have.status(200);
+    expect(tasks).to.eql(response.body);
+  }catch(err) {
+    console.log("Erro: " + err);
+  }
   });
 
-  it('/POST', function (done) {
+  //Teste de criação de tarefa
+  it('/POST', async () => {
     const newTarefa = {
       "title": 'Fazer cafe',
       "description": 'Descrição da tarefa',
       "status": 'In progress'
-    };
-    request(app)
-      .post('/tasks')
-      .send(newTarefa)
-      .end(function (err, response) {
-        if (err) {
-          console.log("Erro: " + err);
-          return done(err);
-        }
-        id = response.body["_id"];
-        response.should.have.status(201);
-        done(); 
-      });
+    }; 
+    try{
+    const response = await request(app)
+    .post('/tasks')
+    .send(newTarefa)
+    id = response.body["_id"];
+    response.should.have.status(201);
+    expect(newTarefa["title"]).to.eql(response.body["title"]);
+    }catch(err) {
+      console.log("Erro: " + err);
+      id = response.body["_id"];
+
+    }
   });
 
-  it('/PUT', function (done) {
+  //Teste de update de tarefa
+  it('/PUT', async () => { 
     let taskUpdate = {
-      "description": "De tarde",
-      "status": "Concluida",
-    };
+        "description": "De tarde",
+        "status": "Concluida",
+    }
     const resposta = "Task atualizada com sucesso!";
-    request(app)
+    try{  
+      const response = await request(app)
       .put(`/tasks/${id}`)
       .send(taskUpdate)
-      .end(function (err, response) {
-        if (err) {
-          console.log("Erro: " + err);
-          return done(err);
-        }
-        response.should.have.status(200);
-        expect(resposta).to.eql(response.text);
-        done();
-      });
+      response.should.have.status(200);
+      expect(resposta).to.eql(response.text);
+    }catch(error){
+      console.log("Erro:" + error);
+    }
   });
 
-  it('/DELETE', function (done) {
+  //Teste de delete de tarefa
+  it('/DELETE', async () => {
     let taskDelete = {
       "title": "Fazer cafe",
-    };
-    const resposta = 'Task excluida com sucesso!';
-    request(app)
-      .delete(`/tasks/${id}`)
-      .send(taskDelete)
-      .end(function (err, response) {
-        if (err) {
-          console.log("Erro: " + err);
-          return done(err);
-        }
-        response.should.have.status(200);
-        expect(resposta).to.eql(response.text);
-        done();
-      });
-  });
+  }
+  const resposta = 'Task excluida com sucesso!';
+  try{
+  const response = await request(app)
+  .delete(`/tasks/${id}`)
+  .send(taskDelete)  
+  response.should.have.status(200);
+  expect(resposta).to.eql(response.text);
+  }catch(error){
+    console.log("Erro:" + error);
+  }
+});
 
 });
