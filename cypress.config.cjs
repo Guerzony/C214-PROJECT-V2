70d60ddc59352c2ae4b30014696cbf35c4cb34e9
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-11-19 12:41:27 -0300

    Relatorio :construction:

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: 'cypress-multi-reporters', // Alterado para usar o cypress-multi-reporters
+  reporterOptions: {
+    configFile: 'reporter-config.json'
+  },
   e2e: {
     setupNodeEvents(on, config) {
-        require('cypress-mochawesome-reporter/plugin')(on)
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
     video: true,
